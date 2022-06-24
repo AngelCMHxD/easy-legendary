b8cbb9d38c65651229e814d5e3813c75db505b83
AngelCMHxD <57822483+AngelCMHxD@users.noreply.github.com>
2022-06-24 00:07:33 -0500

    Remove some unnecessary awaits
    This should improve loading time (not that much)

--- setup.js
+++ setup.js
@@ -51,7 +51,7 @@ module.exports = async () => {
 	let configPath = await getConfigPath();
 
 	if (!fs.existsSync(configPath + "config.json")) {
-		await createConfig();
+		createConfig();
 		await updateLegendary();
 	} else {
 		await checkConfig();
@@ -152,7 +152,7 @@ async function checkConfig() {
 
 	if (config.updateDetected) {
 		await updateLegendary();
-		await configObj.setConfig("updateDetected", false);
+		configObj.setConfig("updateDetected", false);
 	}
 
 	if (Object.entries(config).length !== Object.entries(defaultConfig).length) {
@@ -164,7 +164,7 @@ async function checkConfig() {
 			});
 		});
 
-		await createConfig(prevConfigs);
+		createConfig(prevConfigs);
 	}
 }
 
@@ -181,6 +181,6 @@ async function checkForUpdate() {
 	});
 
 	if (legendary_ver[1] !== release.tag_name) {
-		await configObj.setConfig("updateDetected", true);
+		configObj.setConfig("updateDetected", true);
 	}
 }
