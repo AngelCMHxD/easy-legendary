0206232b2646d48b1d512ed35879951ec5477b68
Bruno <38665405+TheBrunoRM@users.noreply.github.com>
2022-11-14 20:00:37 -0300

    Update getLegendaryPath.js

--- utils/getLegendaryPath.js
+++ utils/getLegendaryPath.js
@@ -1,27 +1,17 @@
 const cp = require("child_process");
 
 module.exports = async () => {
-	let legendaryPath = "";
+	const legendaryPath = cacheObj.legendaryPath;
+	if (legendaryPath) return legendaryPath;
 
-	if (!cacheObj.legendaryPath) {
-		const pipScripts = await cp.execSync(`pip show legendary-gl`, {
-			stdio: "pipe",
-		});
-
-		pipScripts
-			.toString()
-			.split("\n")
-			.forEach((line) => {
-				if (line.includes("Location:")) {
-					let packagesPath = line.split("Location: ")[1];
-					legendaryPath =
-						packagesPath.split("\\").slice(0, -2).join("\\") +
-						"\\scripts\\legendary.exe";
-				}
-			});
-
-		cacheObj.legendaryPath = legendaryPath;
-	} else legendaryPath = cacheObj.legendaryPath;
-
-	return legendaryPath;
+	const pipScripts = await cp.execSync(`pip show legendary-gl`, {
+		stdio: "pipe",
+	}).then(text => {
+		const line = text.toString().split("\n").find(line => line.includes("Location:"));
+		if(!line) return null;
+		let packagesPath = line.split("Location: ")[1];
+		legendaryPath =
+			packagesPath.split("\\").slice(0, -2).join("\\") +
+			"\\scripts\\legendary.exe";
+	});
 };
