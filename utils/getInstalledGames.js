e0d7439e6b348bbebe7be59bcec0c5e71b7329d8
Bruno <38665405+TheBrunoRM@users.noreply.github.com>
2022-11-14 19:56:38 -0300

    Update getInstalledGames.js

--- utils/getInstalledGames.js
+++ utils/getInstalledGames.js
@@ -1,19 +1,20 @@
 const cp = require("child_process");
 
 module.exports = async () => {
-	let games;
-	if (!cacheObj.installedGamesLog) {
-		games = await cp.execSync("legendary list-installed", {
-			stdio: "pipe",
-		});
-		games = games
+	const games = cacheObj.installedGamesLog;
+	if (games) return games;
+	
+	await cp.execSync("legendary list-installed", {
+		stdio: "pipe",
+	}).then(text => {
+		cacheObj.installedGamesLog = text
 			.toString()
-			.replaceAll(
-				'\n  - This game can be activated directly on your Ubisoft account and does not require legendary to install/run. Use "legendary activate --uplay" and follow the instructions.',
-				""
-			)
+			.replaceAll([
+				`\n  - This game can be activated directly on your Ubisoft account`,
+				`and does not require legendary to install/run.`,
+				`Use "legendary activate --uplay" and follow the instructions.`,
+			].join(" "), "")
+			// replaces unicode characters
 			.replaceAll(/[^\x00-\x7F]/g, "");
-		cacheObj.installedGamesLog = games;
-	} else games = cacheObj.installedGamesLog;
-	return games;
+	})
 };
